d52934395af7c8bb27260e61951c08d99ffc8ac3
12MGL <makz.ars@gmail.com>
2024-04-04 17:09:58 +0300

    Update Home_animals.sql
    объединил insert'ы в один, но с SET ещё нужно разбираться

--- Home_animals.sql
+++ Home_animals.sql
@@ -19,47 +19,29 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(50) NOT NULL,
 type VARCHAR(50) NOT NULL,
 birthdate DATE,
-commands SET('command1', 'command2', 'command3', 'command4', 'command5')
+commands SET('Bark', 'Bray', 'Canter', 'Carry Load', 'Fetch', 'Gallop', 'Hide', 'Jump', 'Kick', 'Meow', 'Paw', 'Pounce', 'Roll', 'Sit', 'Stay', 'Spin', 'Scratch', 'Trot', 'Walk')
 );
 
-insert into Pets (name, type, birthdate) VALUES
-('Fido',		'Dog', 		20200101),
-('Whiskers',	'Cat',		20190515),
-('Hammy',		'Hamster',	20210310),
-('Buddy',		'Dog',		20181210),
-('Smudge',		'Cat',		20200220),
-('Peanut',		'Hamster',	20210801),
-('Bella',		'Dog',		20191111),
-('Oliver',		'Cat',		20200630);
+insert into Pets (name, type, birthdate, commands) VALUES
+('Fido',		'Dog', 		20200101, concat('Sit, Stay, Fetch')),
+('Whiskers',	'Cat',		20190515, 'Sit, Pounce'),
+('Hammy',		'Hamster',	20210310, 'Roll, Hide'),
+('Buddy',		'Dog',		20181210, 'Sit, Paw, Bark'),
+('Smudge',		'Cat',		20200220, 'Sit, Pounce, Scratch'),
+('Peanut',		'Hamster',	20210801, 'Roll,Spin'),
+('Bella',		'Dog',		20191111, 'Sit, Stay, 	Roll'),
+('Oliver',		'Cat',		20200630, 'Meow, Scratch, Jump')
+;
 
-insert into Pets (commands) VALUES
-('Sit', 	'Stay', 	'Fetch'),
-('Sit', 	'Pounce'),
-('Roll', 	'Hide'),
-('Sit', 	'Paw', 		'Bark'),
-('Sit', 	'Pounce', 	'Scratch'),
-('Roll',	'Spin'),
-('Sit', 	'Stay', 	'Roll'),
-('Meow', 	'Scratch', 	'Jump');
 
-insert into Pack_animals (name, type, birthdate) VALUES
-('Thunder',	'Horse',	20150721),
-('Sandy',	'Camel',	20161103),
-('Eeyore',	'Donkey',	20170918),
-('Storm',	'Horse',	20140505),
-('Dune',	'Camel',	20181212),
-('Burro',	'Donkey',	20190123),
-('Blaze',	'Horse',	20160229),
-('Sahara',	'Camel',	20150814);
-
-insert into Pack_animals (commands) VALUES
-('Trot', 'Canter', 		'Gallop'),
-('Walk', 'Carry Load'),
-('Walk', 'Carry Load', 	'Bray'),
-('Trot', 'Canter'),
-('Walk', 'Sit'),
-('Walk', 'Bray', 		'Kick'),
-('Trot', 'Jump', 		'Gallop'),
-('Walk', 'Run');
+insert into Pack_animals (name, type, birthdate, commands) VALUES
+('Thunder',	'Horse',	20150721, 'Trot, Canter, 		Gallop'),
+('Sandy',	'Camel',	20161103, 'Walk, Carry Load'),
+('Eeyore',	'Donkey',	20170918, 'Walk, Carry Load, 	Bray'),
+('Storm',	'Horse',	20140505, 'Trot, Canter'),
+('Dune',	'Camel',	20181212, 'Walk, Sit'),
+('Burro',	'Donkey',	20190123, 'Walk, Bray, 		Kick'),
+('Blaze',	'Horse',	20160229, 'Trot, Jump, 		Gallop'),
+('Sahara',	'Camel',	20150814, 'Walk, Run');
 
 SELECT * FROM Pets;
